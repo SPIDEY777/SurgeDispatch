745295cd0ef077eae2d905435d4640536061be65
Spidey777 <s733749@gmail.com>
2026-08-19 18:58:26 +0530

    intial project structure created

---
+++ surge-dispatch/src/main/java/com/surgedispatch/SurgeDispatchApplication.java
@@ -0,0 +1,13 @@
+package com.surgedispatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurgeDispatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurgeDispatchApplication.class, args);
+	}
+
+}
